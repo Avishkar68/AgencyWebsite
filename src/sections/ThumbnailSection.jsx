43a9fb8559c4f3c4c3b05512f66bf9d4ef701bdf
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2025-03-01 20:44:19 +0530

    thumbnailsectionfileadd

---
+++ src/sections/ThumbnailSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ThumbnailSection = () => {
+  return (
+    <div className="w-screen min-h-screen border md:px-36">
+        <div className='border w-screen md:w-full h-screen'>sdfsd</div>
+    </div>
+  )
+}
+
+export default ThumbnailSection
